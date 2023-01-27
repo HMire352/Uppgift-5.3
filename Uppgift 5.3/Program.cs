e830970d2634f9611f5e7cc7df786e06ebb27840
Elev <Elev@TEB125110>
2023-01-27 14:21:01 +0100

    Add project files.

---
+++ Uppgift 5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_5._3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            {
+                int[]xtal = new int[3];
+                Console.WriteLine("skriv in 3 tal");
+
+               xtal[0] = int.Parse(Console.ReadLine());
+               xtal[1] = int.Parse(Console.ReadLine());
+               xtal[2] = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("summan av dina tal " + (xtal[0] + xtal[1] + xtal[2]));
+
+
+            }
+            
+        }
+
+    }
+}
